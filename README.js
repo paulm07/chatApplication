ec092f07240456520a4dbff5f8adf985b25725a5
paulrmolina <paulrmolina@gmail.com>
2016-05-06 21:28:23 -0400

    Added README
    Stuff

--- README.js
+++ README.js
@@ -1,3 +1,19 @@
-/*
-* This chat application was created by Vania Jarquin
+/* This chat application was created by Paul Molina and Vania Jarquin
+
+Commands:
+/list (display channel list)
+/list xxx (display channel that contains that string xxx)
+/msg nick "mesage this" (sends private message to user). Once they are in the chat window, the chat windows takes care of the private messages (using the same emit command)
+/nick mynewnick (changes nickname) 
+/quit 
+/join channel (joins channel and opens new window)
+/leave channel (leaves channel and closes window) 
+/createChannel 
+/removeChannel
+/promote nick
+/demote nick
+/login illuminate (For Admin)
+
+\:Time for time
+\:Weather <city name or zip> for forecast
 */
